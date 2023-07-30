5abbd1600e0fe9c723dbaab6058ce9ebf1b4c5a0
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-30 15:08:22 +0530

    Add footer styling
    Update style.css
    - Add footer styling
    
    Update footer.js
    - Add class to footer

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,9 @@ export function footerContent() {
     const footer = document.createElement("div");
     const footerText = document.createElement("p");
 
-    footerText.textContent = `2023 VMadhuranga`;
+    footer.classList.add("footer");
+
+    footerText.textContent = "\u00A9 2023 VMadhuranga";
     footer.appendChild(footerText);
 
     return footer;
