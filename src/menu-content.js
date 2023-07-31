3d9d39a6066b5d1fba258c4a638b63c2d707dbb4
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-31 17:58:05 +0530

    Add styling to menu content
    Update menu-content.js
    - Add class to menuContainer and menuItemCard
    - Add alt to menuItemImage
    - Add new menu item
    
    Update style.css
    - Update .main-container styling
    - Add menu content styling
    - Add z index to header container

--- src/menu-content.js
+++ src/menu-content.js
@@ -1,21 +1,25 @@
 import vanillaCupcake from "./menu-item-images/vanilla-cupcake.jpg";
 import chocolateCupcake from "./menu-item-images/chocolate-cupcake.jpg";
 import redVelvetCupcake from "./menu-item-images/red-velvet-cupcake.jpg";
+import blueBerryCupcake from "./menu-item-images/blue-berry-cupcake.jpg";
 
 export function menuContent() {
     const menuContainer = document.createElement("div");
-    const menuItemCard = document.createElement("div");
+    menuContainer.classList.add("menu-container");
     
-    let menuItemImageArray = [chocolateCupcake, vanillaCupcake, redVelvetCupcake];
-    let menuItemNameArray = ["Chocolate", "Vanilla", "Red velvet"];
-    let menuItemPriceArray = ["$5", "$6", "$10"];
+    let menuItemImageArray = [chocolateCupcake, vanillaCupcake, redVelvetCupcake, blueBerryCupcake];
+    let menuItemNameArray = ["Chocolate", "Vanilla", "Red velvet", "Blue berry"];
+    let menuItemPriceArray = ["$5", "$6", "$10", "$10"];
     
     for (let i = 0; i < menuItemNameArray.length; i++) {
-        const menuItemImage = new Image(100, 100);
+        const menuItemCard = document.createElement("div");
+        const menuItemImage = new Image();
         const menuItemName = document.createElement("h3");
         const menuItemPrice = document.createElement("p");
-
+        
+        menuItemCard.classList.add("menu-item-card");
         menuItemImage.src = menuItemImageArray[i];
+        menuItemImage.alt = menuItemNameArray[i]
         menuItemName.textContent = menuItemNameArray[i];
         menuItemPrice.textContent = menuItemPriceArray[i];
 
