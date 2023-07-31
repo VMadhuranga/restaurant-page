024daeae86cce8bf4522a8ba02946e90269529a9
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-31 18:32:57 +0530

    Create contact content
    Create contact-content.js
    
    Update contact-content.js
    - Create function contactContent
      - This function will create contact content
    
    Update index.js
    - Import function contactContent

---
+++ src/contact-content.js
@@ -0,0 +1,19 @@
+export function contactContent() {
+    const contactContainer = document.createElement("div");
+    const contactTitle = document.createElement("h1");
+    const telePhone = document.createElement("p");
+    const email = document.createElement("p");
+    const address = document.createElement("p");
+
+    contactTitle.textContent = "Contact Us";
+    telePhone.textContent = "Phone: +94 (123) 123-1234";
+    email.textContent = "Email: fakecupcakes@example.com";
+    address.textContent = "Address: 123 Any street, Any town, SL";
+
+    contactContainer.appendChild(contactTitle);
+    contactContainer.appendChild(telePhone);
+    contactContainer.appendChild(email);
+    contactContainer.appendChild(address);
+
+    return contactContainer;
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { footerContent } from "./footer-content";
 import { mainContent } from "./main-content";
 import { homeContent } from "./home-content";
 import { menuContent } from "./menu-content";
+import { contactContent } from "./contact-content";
 import "../src/style.css";
 
 function pageLoad() {
     const content = document.querySelector("#content");
 
     content.appendChild(headerContent());
-    content.appendChild(mainContent(menuContent()));
+    content.appendChild(mainContent(contactContent()));
     content.appendChild(footerContent());
 }
 
